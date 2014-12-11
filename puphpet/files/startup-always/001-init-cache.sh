eb9f83e3d8a38700f8b7b0b03688360d412f7408
Stéphane Escandell <stephane.escandell@gmail.com>
2014-12-11 12:41:03 +0100

    Force permissions on temp dir

--- puphpet/files/startup-always/001-init-cache.sh
+++ puphpet/files/startup-always/001-init-cache.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ ! -d /tmp/generator ]; then
 	mkdir /tmp/generator
-	chmod g+ws /tmp/generator
-	chown `whoami`:www-data /tmp/generator
 fi
+
+chmod g+ws /tmp/generator
+chown vagrant:www-data /tmp/generator
