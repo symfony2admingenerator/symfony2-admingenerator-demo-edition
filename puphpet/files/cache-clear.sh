d7fc7dbab049ffc588d9170089c8cf3b34638b42
loostro <loostro@gmail.com>
2015-03-25 13:53:39 +0100

    Update cache-clear.sh

--- puphpet/files/cache-clear.sh
+++ puphpet/files/cache-clear.sh
@@ -11,20 +11,15 @@ fi
 
 echo 'Initializing cache'
 
+. "${PUPPET_DIR}"/files/fix-permissions.sh
+
 php bin/console cache:clear -e=dev --no-warmup --no-optional-warmers
 php bin/console cache:clear -e=prod --no-warmup --no-optional-warmers
 
 php bin/console cache:warmup -e=dev
 php bin/console cache:warmup -e=prod
 
-
-if [ `whoami` = 'root' ]; then
-    if [ ! -d "/tmp/${SYMFONY__VAGRANT__ENV}" ]; then
-        mkdir "/tmp/${SYMFONY__VAGRANT__ENV}"
-    fi
-    chown -R vagrant: "/tmp/${SYMFONY__VAGRANT__ENV}"
-    find "/tmp/${SYMFONY__VAGRANT__ENV}" -type d -exec chmod 775 {} \; -exec chmod g+s {} \;
-fi
+. "${PUPPET_DIR}"/files/fix-permissions.sh
 
 echo 'Restarting Apache'
 sudo /etc/init.d/apache2 restart
