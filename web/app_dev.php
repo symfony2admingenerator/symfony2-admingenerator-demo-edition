5dc81b06fe03d8ac08eea82a31e17a4641ef3d9b
Stéphane Escandell <stephane.escandell@gmail.com>
2016-01-30 17:11:34 +0100

    Update to Symfony3.0

--- web/app_dev.php
+++ web/app_dev.php
@@ -18,10 +18,11 @@ if (!array_key_exists('SYMFONY__VAGRANT__ENV', $_SERVER) && (
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
+/**
+ * @var Composer\Autoload\ClassLoader $loader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
-
-require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
